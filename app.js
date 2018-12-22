02c74d0ee613907a8953e5f1578e841953a79516
Andy0414 <pjh8667@naver.com>
2018-12-22 17:10:32 +0900

    UPDATE NPM

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
+const cookieParser = require('cookie-parser')
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.use(express.json()); // body parser
+app.use(express.urlencoded({ extended: false })); // body parser
+app.use(cookieParser()); // 쿠키파서
+app.use(express.static('public')); // 정적 파일
 
 app.listen(3000,()=>{
     console.log("Serve OPEN");
 })
+
